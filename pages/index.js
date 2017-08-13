a78797c69e26de6d3540d302730e002d3d929355
Victor <victorasantekyei@yahoo.co.uk>
2017-08-13 10:40:13 +0200

    ug

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import React from 'react'
 import Page from '../components/page'
 import Layout from '../components/layout'
-import { Row, Col, Card, Carousel } from 'antd'
+import {Row, Col, Card, Carousel} from 'antd'
 import ImgCard from '../modules/imgcard.js'
 import SmCard from '../modules/smcard.js'
 import GridCard from '../modules/gridcard.js'
@@ -28,7 +28,7 @@ export default class extends Page {
     // If running on server, perform Async call
     if (typeof window === 'undefined') {
       props.incentives = await AsyncData.getData()
-      console.log(props.incentives[50].program["$"].split(" ")[0], 'program name do not delete')
+      console.log(props.incentives[50].program.$.split(' ')[0], 'program name do not delete')
       console.log(props.incentives[0], 'second')
     }
 
@@ -55,44 +55,44 @@ export default class extends Page {
 
   render() {
     return (
-      <Layout session={ this.props.session }>
-        <div style={ { background: '#e9e9e9', padding: '0px' } }>
-          <Row gutter={ 30 }>
-            <Col span={ 8 } lg={ 8 } md={ 24 } sm={ 24 } xs={ 24 }> Gutscheine
+      <Layout session={this.props.session}>
+        <div style={{background: '#e9e9e9', padding: '0px'}}>
+          <Row gutter={30}>
+            <Col span={8} lg={8} md={24} sm={24} xs={24}> Gutscheine
             { this.state.incentives.slice(0, 5).map((incentive, i) => (
-                <div key={ i }>
-                  <BasicCard title={ incentive.program["$"].split(" ")[0] + ' Gutschein' } description={incentive.admedia.admediumItem.title} />
+              <div key={i}>
+                  <BasicCard title={incentive.program.$.split(' ')[0] + ' Gutschein'} description={incentive.admedia.admediumItem.title}/>
                 </div>
               )) }
             </Col>
-            <Col span={ 8 } lg={ 8 } md={ 24 } sm={ 24 } xs={ 24 }> Gutscheine
+            <Col span={8} lg={8} md={24} sm={24} xs={24}> Gutscheine
             { this.state.incentives.slice(0, 5).map((incentive, i) => (
-                <div key={ i }>
-                <BasicCard title={ incentive.program["$"].split(" ")[0] + ' Gutschein' } description={incentive.admedia.admediumItem.title} />
+              <div key={i}>
+                  <BasicCard title={incentive.program.$.split(' ')[0] + ' Gutschein'} description={incentive.admedia.admediumItem.title}/>
                 </div>
               )) }
             </Col>
-            <Col span={ 8 } lg={ 8 } md={ 24 } sm={ 24 } xs={ 24 }> Gutscheine
+            <Col span={8} lg={8} md={24} sm={24} xs={24}> Gutscheine
             { this.state.incentives.slice(0, 5).map((incentive, i) => (
-                <div key={ i }>
-                <BasicCard title={ incentive.program["$"].split(" ")[0] + ' Gutschein' } description={incentive.admedia.admediumItem.title} />
+              <div key={i}>
+                  <BasicCard title={incentive.program.$.split(' ')[0] + ' Gutschein'} description={incentive.admedia.admediumItem.title}/>
                 </div>
               )) }
             </Col>
           </Row>
         </div>
         <br/>
-        <Row type="flex" gutter={ 24 } align="middle">
-          <Col lg={ 18 } md={ 24 } sm={ 24 } xs={ 24 }>
-          <Slider effect="fade" sliderImage={ 'http://via.placeholder.com/1200x450' } />
-          <br/>
+        <Row type="flex" gutter={24} align="middle">
+          <Col lg={18} md={24} sm={24} xs={24}>
+            <Slider effect="fade" sliderImage={'http://via.placeholder.com/1200x450'}/>
+            <br/>
           </Col>
-          <Col lg={ 6 } md={ 24 } sm={ 24 } xs={ 24 }>
-          <Row gutter={ 24 } justify="space-around">
+          <Col lg={6} md={24} sm={24} xs={24}>
+            <Row gutter={24} justify="space-around">
             { this.state.incentives.slice(0, 1).map((incentive, i) => (
-                <Col lg={ 24 } md={ 8 } sm={ 12 } xs={ 24 }>
-                <div key={ i }>
-                  <SmCard title={ incentive.name } extra={ <a href="">mehr</a> } description={ incentive.name } />
+              <Col lg={24} md={8} sm={12} xs={24}>
+                  <div key={i}>
+                  <SmCard title={incentive.name} extra={<a href="">mehr</a>} description={incentive.name}/>
                   <br/>
                 </div>
                 </Col>
@@ -100,37 +100,37 @@ export default class extends Page {
           </Row>
           </Col>
           { this.state.incentives.slice(0, 8).map((incentive, i) => (
-              <Col lg={ 6 } md={ 12 }>
-              <div key={ i }>
-                <SmCard title={ incentive.name } extra={ <a href="">mehr</a> } description={ incentive.name } />
+            <Col lg={6} md={12}>
+                <div key={i}>
+                <SmCard title={incentive.name} extra={<a href="">mehr</a>} description={incentive.name}/>
                 <br/>
               </div>
               </Col>
             )) }
-          <Col lg={ 24 } md={ 24 } sm={ 24 } xs={ 24 }>
-          <Card loading title="Card title" style={ { width: '100%' } }>
+          <Col lg={24} md={24} sm={24} xs={24}>
+            <Card loading title="Card title" style={{width: '100%'}}>
             Whatever content
           </Card>
           </Col>
-          <Col lg={ 8 } md={ 24 } sm={ 24 } xs={ 24 }>
-          <Card loading title="Card title" style={ { width: '100%' } }>
+          <Col lg={8} md={24} sm={24} xs={24}>
+            <Card loading title="Card title" style={{width: '100%'}}>
             Whatever content
           </Card>
           </Col>
-          <Col lg={ 8 } md={ 24 } sm={ 24 } xs={ 24 }>
-          <Card loading title="Card title" style={ { width: '100%' } }>
+          <Col lg={8} md={24} sm={24} xs={24}>
+            <Card loading title="Card title" style={{width: '100%'}}>
             Whatever content
           </Card>
           </Col>
-          <Col lg={ 8 } md={ 24 } sm={ 24 } xs={ 24 }>
-          <Card loading title="Card title" style={ { width: '100%' } }>
+          <Col lg={8} md={24} sm={24} xs={24}>
+            <Card loading title="Card title" style={{width: '100%'}}>
             Whatever content
           </Card>
           </Col>
           { this.state.incentives.slice(0, 8).map((incentive, i) => (
-              <Col lg={ 6 } md={ 12 }>
-              <div key={ i }>
-                <SmCard title={ incentive.name } extra={ <a href="">mehr</a> } description={ incentive.name } />
+            <Col lg={6} md={12}>
+                <div key={i}>
+                <SmCard title={incentive.name} extra={<a href="">mehr</a>} description={incentive.name}/>
                 <br/>
               </div>
               </Col>
